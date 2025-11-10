764174af94dd7dc5ba1264d0c1c3fe8018a2771b
Martin Arusalu <martin.arusalu@gmail.com>
2025-11-10 15:23:16 +0200

    More fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import SchemaBuilder from "@/pages/SchemaBuilder";
 import NotFound from "@/pages/not-found";
 
 function Router() {
+  const base = import.meta.env.BASE_URL;
+  
   return (
-    <Switch>
-      <Route path="/" component={SchemaBuilder} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={base}>
+      <Switch>
+        <Route path="/" component={SchemaBuilder} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
